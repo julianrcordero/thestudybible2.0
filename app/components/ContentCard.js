8f7180862d76d199113a78ede2b0de3ba7350a63
Julian Cordero <julianrcordero@gmail.com>
2021-02-08 09:59:38 -0800

    no message

---
+++ app/components/ContentCard.js
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+  View,
+  Image as RNImage,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Image } from "react-native-expo-image-cache";
+
+import Text from "./Text";
+import colors from "../config/colors";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+function ContentCard({
+  category,
+  title,
+  scripture,
+  imageUrl,
+  itemDate,
+  onPress,
+  thumbnailUrl,
+}) {
+  return (
+    <TouchableHighlight onPress={onPress}>
+      <View style={styles.contentCard}>
+        {thumbnailUrl ? (
+          <Image
+            style={styles.image}
+            tint="light"
+            preview={{ uri: thumbnailUrl }}
+            uri={imageUrl}
+          />
+        ) : (
+          <RNImage
+            style={styles.image}
+            source={require("../assets/gtylogo.jpg")}
+          />
+        )}
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title} numberOfLines={1}>
+            {title}
+          </Text>
+          <View
+            style={{
+              alignItems: "center",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text style={styles.scripture} numberOfLines={2}>
+              {scripture}
+            </Text>
+            <Text style={styles.date}>{itemDate}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentCard: {
+    borderWidth: 0.2,
+    borderRadius: 10,
+    backgroundColor: "green",
+    flexDirection: "row",
+    marginVertical: 10,
+    overflow: "hidden",
+  },
+  date: {
+    // backgroundColor: "green",
+    color: colors.secondary,
+    fontSize: 12,
+    fontStyle: "italic",
+  },
+  detailsContainer: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  image: {
+    alignSelf: "center",
+    aspectRatio: 1.2,
+    backgroundColor: "green",
+    width: 150,
+    height: undefined,
+  },
+  scripture: {
+    color: colors.secondary,
+    // fontWeight: "bold",
+  },
+  title: {
+    color: colors.secondary,
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "bold",
+    paddingBottom: 5,
+    // marginBottom: 7,
+  },
+});
+
+export default ContentCard;
